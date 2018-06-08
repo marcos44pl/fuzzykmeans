c7ea8aa73293a132817b19551cdd46678bd9beb0
Marcin <marcknap@gmail.com>
2018-06-08 14:27:29 +0200

    Working solution with plots

--- fuzzy.py
+++ fuzzy.py
@@ -7,7 +7,9 @@ from FuzzyKMeans import FuzzyKMeans
 import matplotlib.pyplot as plt
 
 files_pattern = "./RM/*.html"
-punctuation_pattern = ' |\.$|\. |, |\/|\(|\)|\'|\"|\!|\?|\+|\n|-'
+METRIC_METHOD = "euc"
+K = 4
+M = 1.3
 
 
 def create_bag_of_words():
@@ -19,7 +21,7 @@ def create_bag_of_words():
             soup = BeautifulSoup(html_text, "html5lib")
             all_p = [tag.text for tag in soup.findAll('p')]
             text = ' '.join(all_p)
-            texts.append(text)  # [w for w in re.split(punctuation_pattern, text) if w]
+            texts.append(text)
 
     return vectorizer.fit_transform(texts)
 
@@ -31,27 +33,47 @@ def cos_sim(a, b):
     return dot_product / (norm_a * norm_b)
 
 
+def euclidean_sim(a, b):
+    r = np.subtract(a, b)
+    rp = np.power(r, 2)
+    sum_rp = np.sum(rp)
+    return np.sqrt(sum_rp)
+
+
+sim_func = \
+    {
+        "cos": cos_sim,
+        "euc": euclidean_sim
+    }
+
+
+def sim(a, b, metric):
+    return sim_func[metric](a, b)
+
+
+def norm(a):
+    sum_d = np.sum(a)
+    return np.divide(a, sum_d)
+
+
 if __name__ == '__main__':
     bow = create_bag_of_words()
     vec_0 = np.array([sys.float_info.epsilon for i in range(bow.shape[1])])
     dist_from_0 = []
-    k = 3
-    fuzzy = FuzzyKMeans(k)
+    fuzzy = FuzzyKMeans(K, M)
     X = bow.toarray()
     fuzzy.fit(X)
     dbg = 0
     for vec in bow:
         vec_1d = vec.toarray().reshape(-1)
-        dist_from_0.append(cos_sim(vec_1d, vec_0))
-
+        dist_from_0.append(sim(vec_1d, vec_0, METRIC_METHOD))
     centers = []
     for c in fuzzy.cluster_centers_:
-        centers.append(cos_sim(c, vec_0))
+        centers.append(sim(c, vec_0, METRIC_METHOD))
 
+    plot_colors = 'rygcmkw'
     plt.plot(dist_from_0, [0 for i in range(len(dist_from_0))], 'b.')
-    plt.plot(dist_from_0, fuzzy.fuzzy_labels_[:, 0], 'r.')
-    plt.plot(dist_from_0, fuzzy.fuzzy_labels_[:, 1], 'y.')
-    plt.plot(dist_from_0, fuzzy.fuzzy_labels_[:, 2], 'g.')
-    plt.plot(centers, [0 for i in range(len(centers))], 'k+')
-
+    for ik in range(K):
+        plt.plot(dist_from_0, fuzzy.fuzzy_labels_[:, ik], plot_colors[ik] + '.')
+    plt.plot(centers, [0.1 * i for i in range(len(centers))], 'k+')
     plt.show()
